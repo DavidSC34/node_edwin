ae47d27a78261ff4f4355a9940b5e79ca3f5bfe4
DavidSC <david@wbcboxing.com>
2019-12-16 17:38:23 -0600

    Lesson 4-6 Metodo GET  yPOST con ejemplo de envio de un form, cabeceras en el header

--- http-request-json.js
+++ http-request-json.js
@@ -11,13 +11,17 @@ http.createServer((req, serverRes)=>{
 
                                 httpRes.on('data', data=>{
                                         httpRes.setEncoding('utf8');
-                                        console.log(data);
+                                       serverRes.write(data);
                                 });
                         httpRes.on('end',()=>{
                                 serverRes.end();
                                 console.log('Its over');
                         });
                 });
+        }else{
+
+                serverRes.writeHead(404,{'Content-Type':'text-plain'});
+                serverRes.end('404 ERROR, could not find what you were looking for');
         }
 
 }).listen(4444);
